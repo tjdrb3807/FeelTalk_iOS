199e716c91d9a9751b1c6830014cb6702bd68271
Jeon Seong Gyu <80459245+tjdrb3807@users.noreply.github.com>
2023-06-07 02:02:27 +0900

    FEAT: Create project

---
+++ FeelTalk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FeelTalk
+//
+//  Created by 전성규 on 2023/06/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
