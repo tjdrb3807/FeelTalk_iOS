b2803a8892d1ffdcced81246819c4da243ec49b7
doding2 <dragon041414@gmail.com>
2024-08-05 20:28:29 +0900

    Bug-fix: Dark background in chat list view when dark mode

--- FeelTalk/Presentation/Chat/swiftui/ChatListView.swift
+++ FeelTalk/Presentation/Chat/swiftui/ChatListView.swift
@@ -138,6 +138,7 @@ struct ChatListView: View {
                     }
                 }
             }
+            .background(Color.white)
             .rotationEffect(Angle(degrees: 180))
             .scaleEffect(x: -1.0, y: 1.0, anchor: .center)
             .coordinateSpace(name: "scroll")
